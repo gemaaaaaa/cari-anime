57488de649bdc1f40e34a84399126ebe5e33f9d0
Gema <gemasatria65@gmail.com>
2022-07-17 16:14:39 +0700

    feat: add home page

--- application/views/V_Index.php
+++ application/views/V_Index.php
@@ -5,26 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="stylesheet" href="<?= base_url('assets/css/cssreset.css') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/index.css') ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Montserrat:wght@700&display=swap" rel="stylesheet"> 
 
     <title>Cari Anime</title>
 </head>
 <body>
     <input type="hidden" id="baseURL" value="<?= base_url() ?>">
+    
+    <h2 class="brand-name">Cari Anime</h2>
+    
+    <div class="search-bar-input">
+        <input type="text" name="" id="">
+    </div>
 
-    <main>
-        <h3>Cari Anime</h3>
-
-        <div>
-            <input type="text" class="main-search-bar" name="searchBar" id="searchBar">
-            <button>
-                <i class="fa-solid fa-magnifying-glass"></i>
-            </button>
-        </div>
-
-        <div class="list-anime"></div>
-    </main>
+    <div class="search-bar-result"></div>
 </body>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,94 @@
-const baseURL = $('#baseURL');
+const baseURL = $('#baseURL').val();
 
-$('#searchBar').on('input', function(e) {
-    const searchLength = $(this).val().length;
-    const MIN_INPUT = 3;
+const templateHTML = (url, title, img) => {
+    return `
+    <div class="item">
+        <div class="image">
+            <img src="${img}" alt="${title}" height="55px">
+        </div>
+        
+        <div class="title">
+            <a href="${url}" target="_blank">
+                ${title}
+            </a>
+        </div>
+    </div>
+    `
+}
 
-    if (searchLength === MIN_INPUT) {
-        setTimeout(() => {
-            const limit = 10;
-            const searchValue = $(this).val();
-    
-            const templateHTML = (url, title) => {
-                return `
-                <div>
-                    <a href="${url}" target="_blank">${title}</a>
-                </div>
-                `
+$('.search-bar-input input').one('click', () => {
+    const filter = "airing";
+    const limit = 10;
+
+    $('.search-bar-result').show();
+
+    $.ajax({
+        method: "GET",
+        url: `https://api.jikan.moe/v4/top/anime`,
+        data: {
+            filter: filter,
+            limit: limit,
+        },
+        success: (animeResult) => {
+            const {data} = animeResult;
+
+            data.forEach(v => {
+                let url = baseURL + "anime/" + v.mal_id;
+                let title = v.title;
+                let img = v.images.webp.large_image_url;
+
+                const HTML = templateHTML(url, title, img);
+                
+                $('.search-bar-result').append(HTML);
+
+                $('.search-bar-input input').addClass("remove-bottom-border-radius");
+            });
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            let errorStatus = `${textStatus} : ${errorThrown}`;
+
+            console.error(errorStatus)
+        }
+    })
+})
+
+$('.search-bar-input input').on('input', function(e) {
+    const limit = 10;
+    const searchValue = $(this).val();
+
+    if (searchValue === "") return;
+
+    clearTimeout(this.delay);
+
+    this.delay = setTimeout(function(){
+        $.ajax({
+            method: "GET",
+            url: "https://api.jikan.moe/v4/anime",
+            data: {
+                q: searchValue,
+                limit: limit,
+            },
+            success: (animeResult) => {
+                const {data} = animeResult;
+                
+                $('.search-bar-result').empty();
+
+                data.forEach(v => {
+                    let url = baseURL + "anime/" + v.mal_id;
+                    let title = v.title;
+                    let img = v.images.webp.large_image_url;
+
+                    const HTML = templateHTML(url, title, img);
+
+                    $('.search-bar-result').append(HTML);
+                });
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                let errorStatus = `${textStatus} : ${errorThrown}`;
+
+                console.error(errorStatus)
             }
-    
-            $.ajax({
-                method: "GET",
-                url: "https://api.jikan.moe/v4/anime",
-                data: {
-                    q: searchValue,
-                    limit: limit,
-                },
-                success: (animeResult) => {
-                    const {data} = animeResult;
-    
-                    $('.list-anime').empty();
-    
-                    data.forEach(v => {
-                        let url = v.url;
-                        let title = v.title;
-    
-                        const HTML = templateHTML(url, title)
-    
-                        $('.list-anime').append(HTML);
-                    });
-                },
-                error: function(jqXHR, textStatus, errorThrown) {
-                    let errorStatus = `${textStatus} : ${errorThrown}` 
-
-                    console.error(errorStatus)
-                }
-            })
-        }, 1000);   
-    }
+        })
+        
+    }.bind(this), 500);
 })
